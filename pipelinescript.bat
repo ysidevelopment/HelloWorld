bae5b78976ab17862b58cb9243b4ea736dbccf56
ysidevelopment <30556621+ysidevelopment@users.noreply.github.com>
2018-06-23 13:27:48 -0400

    Update pipelinescript.bat

--- pipelinescript.bat
+++ pipelinescript.bat
@@ -1,8 +1,6 @@
 node {
   stage 'Run JMeter Test'
-  sh C:/TestWheel-Phase2/apache-jmeter-3.0/bin/jmeter.bat 
-  -Jjmeter.save.saveservice.output_format=csv -n -t C:/TestWheel-Phase2/apache-jmeter-3.0/bin/Etax2290.jmx -l
-   C:/Users/schakra/.jenkins/workspace/PerformanceTest/report/Etax2290Test.jtl  -e -o 
-   C:/Users/schakra/.jenkins/workspace/PerformanceTest/report/HtmlReports
+  sh C:/TestWheel-Phase2/apache-jmeter-3.0/bin/jmeter.bat  -Jjmeter.save.saveservice.output_format=csv -n -t C:/TestWheel-Phase2/apache-jmeter-3.0/bin/Etax2290.jmx -l
+   C:/Users/schakra/.jenkins/workspace/PerformanceTest/report/Etax2290Test.jtl  -e -o C:/Users/schakra/.jenkins/workspace/PerformanceTest/report/HtmlReports
 
 }
